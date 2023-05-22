1c3a2c8e02362e6529a67e294731d421a9e9118c
Merel Jacobs <merel.burleigh@gmail.com>
2023-05-21 17:04:35 -0700

    worked on reset button and eventListening Styling

--- assets/script.js
+++ assets/script.js
@@ -3,6 +3,7 @@ var startBtn = document.querySelector("#startBtn");
 // make variable for timer
 var timerEl = document.querySelector("#timer");
 // make varibale for printResults section in HTML
+var resutsSection = document.querySelector("#results");
 var printSection = document.querySelector(".printResults");
 var quizTemplate = document.querySelector("#quiz");
 // is this why only one button works?
@@ -18,7 +19,6 @@ var answerContainer1 = document.getElementById('answerOption1');
 var answerContainer2 = document.getElementById('answerOption2');
 var answerContainer3 = document.getElementById('answerOption3');
 var answerContainer4 = document.getElementById('answerOption4');
-var submitButton = document.getElementById('submit');
 
 var questions = [
     {
@@ -47,13 +47,58 @@ var questions = [
     }
 ]
 
+var theQuestions = [
+    {
+        question: "Do you like to code?",
+        answers: {
+            a: 'Yes',
+            b: 'No',
+            c: 'Sometimes'
+        },
+        correctAnswer: 'a'
+    },
+    {
+        question: "Do you eat while you code?",
+        answers: {
+            a: 'Yes',
+            b: 'No',
+            c: 'Sometimes'
+        },
+        correctAnswer: 'c'}
+]
+
+startBtn.addEventListener("click", generateQuiz);
+
+function generateQuiz() {
+    function showQuestions(theQuestions, quizContainer) {
+        var output = [];
+        var answers;
+
+        for (var i = 0; i<theQuestions.length; i++){
+            answers = [];
+            for(letter in theQuestions[i].answers){
+                answers.push(
+                    '<label>' + '<input type="button" name="question'+i+'" value="'+letter+'">' + letter + ': ' + theQuestions.answers[letter] + '</label>'
+                );
+            }
+            output.push(
+                '<div class="question">' + theQuestions[i].question + '</div>' + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+    }
+}
+
+
+
+
+
 // set duration of timer -- done
-var secondsLeft = 5
+var secondsLeft = 5;
 // when you click the start button, the timer begins -- done
 function startQuiz() {
     var quiz = document.querySelector("#quiz");
     quiz.style.display = "flex";
-    restartBtn.setAttribute("style", "display: flex")
+    startBtn.setAttribute("style", "display: none");
 };
 
 function startTimer() {
@@ -70,29 +115,37 @@ function startTimer() {
         quizTemplate.setAttribute("style", "display: none")}
     }, 1000);
 
+
     // when time starts, quiz pops up
 };
 
-// having trouble with print results function // creating HTML lements 
+
 function printResults() {
-    // create a new div in HTML
-    var resultsDiv = document.createElement("div");
+    restartBtn.setAttribute("style", "display: flex");
+    var printSection = document.createElement("p");
     // tell the new div what to say
-    resultsDiv.innerText = "Results";
+    printSection.innerText = "Results";        
     // append the new div to the body of the HTML file
-    document.body.children[3].appendChild(resultsDiv);
-    var resultsDiv = document.createElement("div");
+    document.body.children[2].appendChild(printSection);
+    var printSection = document.createElement("p");        
     // tell the new div what to say
-    resultsDiv.innerText = "Your Score: " + yourScore;
-    // append the new div to the body of the HTML file
-    document.body.children[3].appendChild(resultsDiv);
+    printSection.innerText = "Your Score: " + yourScore;
+    // append the new div to the body of the HTML file 
+    document.body.children[2].appendChild(printSection);
     var initalsDiv = document.createElement("input");
-    initalsDiv.setAttribute("placeholder", "Input Initals");
-    document.body.children[3].appendChild(initalsDiv);
+    initalsDiv.setAttribute("placeholder", "Input Initals");        
+    printSection.appendChild(initalsDiv);
+    printSection.setAttribute("style", "display: flex; flex-direction: column; text-align: center");
+    initalsDiv.setAttribute("style", "text-align: center;");
+    // if you type in the input box, it will save the initals in local storage
     initalsDiv.addEventListener("change", function storeInitals() {
         localStorage.setItem("initals", initalsDiv.value);
-    })
+        });
 
+    // show the restart button
+    restartBtn.setAttribute("style", "display: flex");
+    // hide the start button
+    startBtn.setAttribute("style", "display: none");
 };
 
 
@@ -118,17 +171,26 @@ function showNextQuestion() {
 
 var yourScore = 0
 function checkAnswer() {
-    if  (questions[0].possibleAnswer1 === questions[0].correctAnswer) {yourScore += 1}
-    else if (questions[0].possibleAnswer2 === questions[0].correctAnswer) {yourScore += 1}
-    else if (questions[0].possibleAnswer3 === questions[0].correctAnswer) {yourScore += 1}
-    else if (questions[0].possibleAnswer4 === questions[0].correctAnswer) {yourScore += 1}
-    else if (questions[0].possibleAnswer1 !== questions[0].correctAnswer) {console.log("Time Deducted")}
-    else if (questions[0].possibleAnswer2 !== questions[0].correctAnswer) {console.log("Time Deducted")}
-    else if (questions[0].possibleAnswer3 !== questions[0].correctAnswer) {console.log("Time Deducted")}
-    else if (questions[0].possibleAnswer4 !== questions[0].correctAnswer) {console.log("Time Deducted")}
+    if  (answerContainer1.innerText === questions[0].correctAnswer) {yourScore += 1}
+    else if (answerContainer2.innerText === questions[0].correctAnswer) {yourScore += 1}
+    else if (answerContainer3.innerText === questions[0].correctAnswer) {yourScore += 1}
+    else if (answerContainer4.innerText === questions[0].correctAnswer) {yourScore += 1}
+    else if (console.log("Time Deducted"))
 
     console.log(yourScore);
 };
+// function checkAnswer() {
+//     if  (questions[0].possibleAnswer1 === questions[0].correctAnswer) {yourScore += 1}
+//     else if (questions[0].possibleAnswer2 === questions[0].correctAnswer) {yourScore += 1}
+//     else if (questions[0].possibleAnswer3 === questions[0].correctAnswer) {yourScore += 1}
+//     else if (questions[0].possibleAnswer4 === questions[0].correctAnswer) {yourScore += 1}
+//     else if (questions[0].possibleAnswer1 !== questions[0].correctAnswer) {console.log("Time Deducted")}
+//     else if (questions[0].possibleAnswer2 !== questions[0].correctAnswer) {console.log("Time Deducted")}
+//     else if (questions[0].possibleAnswer3 !== questions[0].correctAnswer) {console.log("Time Deducted")}
+//     else if (questions[0].possibleAnswer4 !== questions[0].correctAnswer) {console.log("Time Deducted")}
+
+//     console.log(yourScore);
+// };
 
 localStorage.setItem("score", yourScore);
 var pastScore = document.createElement("div");
@@ -153,9 +215,14 @@ pastScore.innerText = "Past Score: " + localStorage.getItem("score")
 
 function restartPage() {
     yourScore = 0;
-    secondsLeft = 10;
+    secondsLeft = 3;
     startTimer();
+    timerEl.setAttribute("style", "display: flex; align-items: flex-end");
+    startQuiz();
+    resutsSection.innerHTML = "";
+    resutsSection.style.display = "none";
     console.log("Page Restarted");
+    restartBtn.setAttribute("style", "display: none");
 };
 
 
@@ -164,8 +231,8 @@ function restartPage() {
 
 startBtn.addEventListener("click", startTimer);
 startBtn.addEventListener("click", startQuiz);
-answerBtn1.addEventListener("click", checkAnswer);
-answerBtn2.addEventListener("click", checkAnswer);
-answerBtn3.addEventListener("click", checkAnswer);
-answerBtn4.addEventListener("click", checkAnswer);
+// answerBtn1.addEventListener("click", checkAnswer);
+// answerBtn2.addEventListener("click", checkAnswer);
+// answerBtn3.addEventListener("click", checkAnswer);
+// answerBtn4.addEventListener("click", checkAnswer);
 restartBtn.addEventListener("click", restartPage);
